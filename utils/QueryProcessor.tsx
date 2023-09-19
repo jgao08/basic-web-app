133bfbfadf2cf4f23eed527bcbf3d4b0cef2f39f
Jacky Gao <jackygao02@andrew.cmu.edu>
2023-09-19 15:44:32 -0400

    fixed name query

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -1,16 +1,13 @@
 export default function QueryProcessor(query: string): string {
-  if (query.toLowerCase().includes("shakespeare")) {
-    return (
-      "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
-      "English poet, playwright, and actor, widely regarded as the greatest " +
-      "writer in the English language and the world's pre-eminent dramatist."
-    );
-  } else if (query.toLowerCase().includes("what is your andrew id")) {
-    return (
-      "my andrewid is jackyg"
-    );
-  }else if (query.toLowerCase().includes("name")){
-    return "jacky"
+  const queries = ["shakespeare", "andrew id", "name"]
+  const responses = ["William Shakespeare (26 April 1564 - 23 April 1616) was an " +
+  "English poet, playwright, and actor, widely regarded as the greatest " +
+  "writer in the English language and the world's pre-eminent dramatist.",  "my andrewid is jackyg", "polly"]
+
+  for (let i = 0; i < queries.length; i++){
+    if (query.toLowerCase().includes(queries[i])){
+      return responses[i]
+    }
   }
 
   return "";
