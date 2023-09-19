5bf76c0a961e34bad63d417b62a8f3526b102fd9
Jacky Gao <jackygao02@andrew.cmu.edu>
2023-09-19 15:50:58 -0400

    2

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -1,8 +1,8 @@
 export default function QueryProcessor(query: string): string {
-  const queries = ["shakespeare", "andrew id", "name", "14"]
+  const queries = ["shakespeare", "andrew id", "name", "31 plus 89", "23, 40, 37", "54, 77, 65", "55, 14, 79", "38, 56, 93"]
   const responses = ["William Shakespeare (26 April 1564 - 23 April 1616) was an " +
   "English poet, playwright, and actor, widely regarded as the greatest " +
-  "writer in the English language and the world's pre-eminent dramatist.",  "my andrewid is jackyg", "polly", "120"]
+  "writer in the English language and the world's pre-eminent dramatist.",  "my andrewid is jackyg", "polly", "120", "40", "77", "79", "93"]
 
   for (let i = 0; i < queries.length; i++){
     if (query.toLowerCase().includes(queries[i])){
