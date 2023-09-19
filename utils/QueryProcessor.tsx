4b839fd516221a252c97506c184e80fb8b27354e
Jacky Gao <jackygao02@andrew.cmu.edu>
2023-09-19 15:53:45 -0400

    5

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -1,15 +1,20 @@
 export default function QueryProcessor(query: string): string {
-  const queries = ["shakespeare", "andrew id", "name", "31 plus 89", "23, 40, 37", "54, 77, 65", "55, 14, 79", "38, 56, 93",
-                    "67 plus 74", "What is 81 plus 18?"]
+  const queries = ["shakespeare", "andrew id", "name"]
   const responses = ["William Shakespeare (26 April 1564 - 23 April 1616) was an " +
   "English poet, playwright, and actor, widely regarded as the greatest " +
-  "writer in the English language and the world's pre-eminent dramatist.",  "my andrewid is jackyg", "polly", 
-  "120", "40", "77", "79", "93", "141", "100"]
+  "writer in the English language and the world's pre-eminent dramatist.",  "my andrewid is jackyg", "polly"]
 
   for (let i = 0; i < queries.length; i++){
     if (query.toLowerCase().includes(queries[i])){
       return responses[i]
     }
+  }
+
+  const addMatch = query.match(/What is (\d+) plus (\d+)/);
+  if (addMatch) {
+    const x: number = parseInt(addMatch[1]);
+    const y: number = parseInt(addMatch[2]);
+    return (x+y).toString();
   }
 
   return "";
